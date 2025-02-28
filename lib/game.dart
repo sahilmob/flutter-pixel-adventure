6ff89b95bf53fe530ca8655c091f253549388fe6
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-02-28 15:42:52 +0300

    add players

--- lib/game.dart
+++ lib/game.dart
@@ -1,20 +1,21 @@
-import 'dart:async';
 import 'dart:ui';
+import 'dart:async';
 
-import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/components.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
 class PixelAdventureGame extends FlameGame {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
-
+  @override
+  final world = Level(levelName: "level-01.tmx");
   late final CameraComponent cam;
-  @override
-  final world = Level();
 
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    await images.loadAllImages();
+
     cam = CameraComponent.withFixedResolution(
       width: 640,
       height: 360,
--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -1,15 +1,33 @@
 import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:pixel_adventure/actors/player.dart';
 
 class Level extends World {
+  final String levelName;
   late TiledComponent level;
+
+  Level({required this.levelName});
 
   @override
   FutureOr<void> onLoad() async {
-    level = await TiledComponent.load("level-01.tmx", Vector2.all(16));
+    level = await TiledComponent.load(levelName, Vector2.all(16));
 
     add(level);
+
+    final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>("Spawnpoints");
+
+    for (final sp in spawnPointsLayer!.objects) {
+      switch (sp.class_) {
+        case "Player":
+          final player = Player(
+            character: "Mask Dude",
+            position: Vector2(sp.x, sp.y),
+          );
+          add(player);
+          break;
+      }
+    }
 
     return super.onLoad();
   }
