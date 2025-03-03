11e590a26bb6b3053557f2eed2eb3491158e1042
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-03-03 23:42:20 +0300

    add vertical collision detection and jump wip

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -14,11 +14,17 @@ base class BasePlayer extends SpriteAnimationGroupComponent
   static double stepTime = 0.05;
   List<CollisionBlock> collisionBlocks = [];
 
+  final double _gravity = 9.8;
+  final double _jumpForce = 460;
+  final double _terminalVelocity = 300;
+
   final String character;
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
 
   double moveSpeed = 100;
+  bool hasJumped = false;
+  bool isOnGround = false;
   double horizontalMovement = 0.0;
   Vector2 velocity = Vector2.zero();
 }
@@ -59,6 +65,8 @@ base mixin PlayerKeyboardHandler on BasePlayer, KeyboardHandler {
         keysPressed.contains(LogicalKeyboardKey.keyD) ||
         keysPressed.contains(LogicalKeyboardKey.arrowRight);
 
+    hasJumped = keysPressed.contains(LogicalKeyboardKey.space);
+
     horizontalMovement += isLeftKeyPressed ? -1 : 0;
     horizontalMovement += isRightKeyPressed ? 1 : 0;
 
@@ -82,12 +90,23 @@ base class Player extends BasePlayer
     _updatePlayerState();
     _updatePlayerMovement(dt);
     _checkHorizontalCollisions();
+    _applyGravity(dt);
+    _checkVerticalCollisions();
     super.update(dt);
   }
 
   void _updatePlayerMovement(double dt) {
+    if (hasJumped && isOnGround) _playerJump(dt);
+
     velocity.x = horizontalMovement * moveSpeed;
     position.x += velocity.x * dt;
+  }
+
+  void _playerJump(double dt) {
+    velocity.y = -_jumpForce;
+    position.y += velocity.y * dt;
+    isOnGround = false;
+    hasJumped = false;
   }
 
   void _updatePlayerState() {
@@ -115,11 +134,39 @@ base class Player extends BasePlayer
           if (velocity.x > 0) {
             velocity.x = 0;
             position.x = b.x - width;
+            break;
           }
           if (velocity.x < 0) {
             velocity.x = 0;
             position.x = b.x + b.width + width;
+            break;
           }
+        }
+      }
+    }
+  }
+
+  void _applyGravity(double dt) {
+    velocity.y += _gravity;
+    velocity.y = velocity.y.clamp(-_jumpForce, _terminalVelocity);
+    position.y += velocity.y * dt;
+  }
+
+  void _checkVerticalCollisions() {
+    for (final b in collisionBlocks) {
+      if (b.isPlatform) {
+      } else {
+        if (checkCollision(this, b)) {
+          if (velocity.y > 0) {
+            velocity.y = 0;
+            position.y = b.y - height;
+            isOnGround = true;
+            break;
+          }
+        }
+        if (velocity.y < 0) {
+          velocity.y = 0;
+          position.y = b.y + b.height;
         }
       }
     }
